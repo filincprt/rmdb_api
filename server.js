5add44823d5fd21fad46e0dc71e4d0d568798720
filin_cprt <61471737+filincprt@users.noreply.github.com>
2024-02-12 17:33:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -427,38 +427,28 @@ app.get('/orders/:id', (req, res) => {
 });
 
 
+
 // Добавление данных в таблицу Orders
 app.post('/orders', (req, res) => {
-    const { user_id, product_id, quantity, order_number, delivery_time, status_id } = req.body;
-    // Проверка наличия товара на складе
-    const queryCheckInventory = 'SELECT quantity FROM Inventory WHERE product_id = ?';
-    db.get(queryCheckInventory, [product_id], (err, row) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        if (!row || row.quantity < quantity) {
-            res.status(400).json({ error: 'Недостаточное количество товара на складе' });
-            return;
-        }
-        // Если достаточное количество товара на складе, добавляем товар в заказ
-        const queryOrder = 'INSERT INTO Orders (user_id, order_number, delivery_time, status_id) VALUES (?, ?, ?, ?)';
-        db.run(queryOrder, [user_id, order_number, delivery_time, status_id], function (err) {
-            if (err) {
-                res.status(500).json({ error: err.message });
-                return;
-            }
-            const orderId = this.lastID;
-            const queryOrderLine = 'INSERT INTO Order_Lines (order_id, product_id, quantity) VALUES (?, ?, ?)';
-            db.run(queryOrderLine, [orderId, product_id, quantity], function (err) {
-                if (err) {
-                    res.status(500).json({ error: err.message });
-                    return;
-                }
-                res.json({ id: orderId });
-            });
-        });
-    });
+  const { user_id, product_id, quantity, order_number, delivery_time, status_id } = req.body;
+  const queryOrder = 'INSERT INTO Orders (user_id, order_number, delivery_time, status_id) VALUES (?, ?, ?, ?)';
+  db.run(queryOrder, [user_id, order_number, delivery_time, status_id], function (err) {
+      if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+      }
+
+      const orderId = this.lastID;
+      const queryOrderLine = 'INSERT INTO Order_Lines (order_id, product_id, quantity) VALUES (?, ?, ?)';
+      db.run(queryOrderLine, [orderId, product_id, quantity], function (err) {
+          if (err) {
+              res.status(500).json({ error: err.message });
+              return;
+          }
+
+          res.json({ id: orderId });
+      });
+  });
 });
 
 
