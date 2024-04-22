b3d6ac3756cbccd925b019c21e94aadcb143a28c
filin_cprt <61471737+filincprt@users.noreply.github.com>
2024-04-22 12:07:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1052,17 +1052,15 @@ app.put('/couriers/:id/toggleStatus', (req, res) => {
 //---------------------PRODUCTS----------------------
 
 app.get('/products', (req, res) => {
-    const lastUpdated = req.query.lastUpdated; // Получаем временную метку последнего обновления с клиента
     const query = `
     SELECT P.id, P.name, P.price, P.color_primary, P.color_light, P.description, P.image_resource, P.quantity, P.units_id, P.barcode, P.category_id, C.nameCategory as category_name, U.name as unit_name, PA.is_available as is_available, P.last_updated
     FROM Products P
     LEFT JOIN Category C ON P.category_id = C.id
     LEFT JOIN UnitsOfMeasurement U ON P.units_id = U.id
     JOIN ProductAvailabilityInShowcase PA ON P.id = PA.product_id
-    WHERE P.last_updated > ?
   `;
 
-  db.all(query, [lastUpdated], (err, rows) => {
+  db.all(query, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
